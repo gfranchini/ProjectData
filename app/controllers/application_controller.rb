d8607d2445891cb3b51cd0b6196f83c0fea04c4f
Giancarlo Franchini <gfranchini88@gmail.com>
2018-02-02 02:56:12 -0500

    Added some devise logout routing logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  add_flash_types :danger, :info, :warning, :success
+
+  private
+  
+  def after_sign_out_path_for(current_user)
+    pages_goodbye_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :environments
   resources :projects
+  get 'pages/home'
+  get 'pages/goodbye'
 
   resources :projects do
     resources :environments
